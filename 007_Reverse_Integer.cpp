1510e49e6d5ba074a765ddea0e97ff8d075a408b
JUIN TAN <tanjuinlong@gmail.com>
2024-06-06 21:04:28 +0800

    add-problem007

---
+++ 007_Reverse_Integer.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int reverse(int x) {
+        if (x < 10 && x > -10)
+            return x;
+
+        long long ans = 0;
+        while (x != 0)
+        {
+            ans *= 10;
+            ans += x % 10;
+            x /= 10;
+            if (ans > INT_MAX || ans < INT_MIN)
+                return 0;
+        }
+        return ans;
+    }
+};
