b6aa3d77806132bffaf871c2d1af4daeb60540d7
JUIN TAN <tanjuinlong@gmail.com>
2024-06-07 22:19:33 +0800

    add-problem-0027

---
+++ 0027_Remove_Element_easy.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        if (nums.empty())
+            return 0;
+
+        int num = 0;
+        int idx = 0;
+
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (nums[i] != val)
+            {
+                nums[idx] = nums[i];
+                idx++;
+                num++; 
+            }
+        }
+
+        return num;
+    }
+};
