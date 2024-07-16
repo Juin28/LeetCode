5f474dbda5d4016e4de0f166daa99af729d0fbba
JUIN TAN <tanjuinlong@gmail.com>
2024-07-16 23:25:03 +0800

    add-problems

--- 0003_Longest_Substring_Without_Repeating_Characters_medium.cpp
+++ 0003_Longest_Substring_Without_Repeating_Characters_medium.cpp
@@ -11,33 +11,30 @@ public:
         int track_p1 = 0;
         int track_p2 = 0;
         int idx[128] {-1};
-        bool track[128] {false};
+        bool seen[128] {false};
 
         for (int i = 0; i < s.length(); i++)
         {
-            if (track[s[i]])
+            if (seen[s[i]])
             {
                 int temp = track_p1;
                 track_p1 = idx[s[i]] + 1;
                 track_p2 = i;
                 for (int j = 0; j < (idx[s[i]] - temp); j++)
                 {
-                    track[s[j + temp]] = false;
-                }
-                for (int k = 0; k <= (idx[s[i]] - temp); k++)
-                {
-                    idx[s[k + temp]] = -1;
+                    seen[s[j + temp]] = false;
+                    idx[s[j + temp]] = -1;
                 }
                 idx[s[i]] = i;
             }
             else
             {
-                track[s[i]] = true;
+                seen[s[i]] = true;
                 idx[s[i]] = i;
                 track_p2 = i;
             }
 
-            if ((track_p2 - track_p1) > (ans_p2 - ans_p1))
+            if ( (track_p2 - track_p1) > (ans_p2 - ans_p1) )
             {
                 ans_p1 = track_p1;
                 ans_p2 = track_p2;
