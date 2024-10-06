921d69fcc26e470c3a54035bc46a1464bfdc321a
Juin28 <tanjuinlong@gmail.com>
2024-10-07 00:02:05 +0800

    add-questions

--- 0000_Automation_Create_File.py
+++ 0000_Automation_Create_File.py
@@ -1,14 +1,15 @@
-def convert_to_filename(input_str):
+def convert_to_filename(input_str, difficulty):
     words = input_str.split()[1:]
 
     index = input_str.split()[0].split('.')[0].zfill(4)
 
     filename = "_".join(words)
 
-    return f"{index}_{filename}.cpp"
+    return f"{index}_{filename}_{difficulty}.cpp"
 
 filename = input("Enter the question: ")
-filename = convert_to_filename(filename)
+difficulty = input("Enter the difficulty: ")
+filename = convert_to_filename(filename, difficulty)
 
 print("Enter the code (press Ctrl+C to finish):")
 code = ""
