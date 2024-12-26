6afffb6c853e15677ac4490a3156421238e7ba26
juin@kth.se Juin28 <tanjuinlong@gmail.com>
2024-12-26 16:37:30 +0800

    add-question

---
+++ 0494_Target_Sum_medium.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int recursion(vector<int>& nums, int target, int index, int sum) {
+        if (index == nums.size()) {
+            return target == sum;
+        }
+
+        return recursion(nums, target, index + 1, sum + nums[index]) + recursion(nums, target, index + 1, sum - nums[index]);
+    }
+
+    int findTargetSumWays(vector<int>& nums, int target) {
+        return recursion(nums, target, 0, 0);
+    }
+};
